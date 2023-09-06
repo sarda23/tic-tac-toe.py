3f1432e5096fe7ddf339dd0366c7a56b60c568e1
sarda23 <nishantsarda23@gmail.com>
2023-09-07 01:54:37 +0530

    tic tac toe console game in python

---
+++ main.py
@@ -0,0 +1,57 @@
+def sum(a,b,c):
+    return a+b+c
+
+def printBoard(xState,zState):
+    zero = 'X' if xState[0] else ('0' if zState[0] else 0)
+    one = 'X' if xState[1] else ('0' if zState[1] else 1)
+    two = 'X' if xState[2] else ('0' if zState[2] else 2)
+    three = 'X' if xState[3] else ('0' if zState[3] else 3)
+    four = 'X' if xState[4] else ('0' if zState[4] else 4)
+    five = 'X' if xState[5] else ('0' if zState[5] else 5)
+    six = 'X' if xState[6] else ('0' if zState[6] else 6)
+    seven = 'X' if xState[7] else ('0' if zState[7] else 7)
+    eight = 'X' if xState[8] else ('0' if zState[8] else 8)
+
+    print(f"{zero} | {one} | {two} ")
+    print(f"--|---|---")
+    print(f"{three} | {four} | {five} ")
+    print(f"--|---|---")
+    print(f"{six} | {seven} | {eight} ")
+    pass
+
+def checkWin(xState,zState):
+    wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],
+             [0,4,8],[2,4,6]]
+    for win in wins:
+        if(sum(xState[win[0]],xState[win[1]],xState[win[2]]) == 3):
+            print("X's won the game")
+            return 1
+        elif(sum(zState[win[0]],zState[win[1]],zState[win[2]]) == 3):
+            print("O's won the game")
+            return 0
+    return -1
+        
+
+if __name__ == "__main__":
+    xState = [0,0,0,0,0,0,0,0,0]
+    zState = [0,0,0,0,0,0,0,0,0]
+    chance = 1 # 1 for x and 0 for O
+    print("welcome to tic tac toe")
+    while(True):
+        printBoard(xState,zState)
+        if(chance):
+            print("X's chance")
+            value = int(input("please enter a value: "))
+            xState[value] = 1
+        else:
+            print("O's chance")
+            value = int(input("please enter a value: "))
+            zState[value] = 1
+        cwin = checkWin(xState,zState)
+
+        if(cwin != -1):
+            break
+
+        chance = 1-chance 
+        
+    
